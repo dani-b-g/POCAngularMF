3cc1cf9d17d6022b7c010efabbc8bb3ca80a9786
Daniel Barriga Grados <dbarriga@viewnext.com>
2023-07-21 09:02:55 +0200

    Proxyconf to oauth

--- projects/mf-auth/src/app/core/core.module.ts
+++ projects/mf-auth/src/app/core/core.module.ts
@@ -1,29 +1,29 @@
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 
-import { SharedModule } from '@app/shared/shared.module';
+// import { SharedModule } from '@app/shared/shared.module';
 
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
 
 const appModules = [BrowserModule, BrowserAnimationsModule];
 @NgModule({
   imports: [
-    SharedModule,
-    ...appModules,
-    TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [HttpClient]
-      }
-    })
+    // SharedModule,
+    // ...appModules,
+    // TranslateModule.forRoot({
+    //   loader: {
+    //     provide: TranslateLoader,
+    //     useFactory: HttpLoaderFactory,
+    //     deps: [HttpClient]
+    //   }
+    // })
   ],
   declarations: [],
   providers: [],
-  exports: [TranslateModule, ...appModules]
+  // exports: [TranslateModule, ...appModules]
 })
 export class CoreModule {
   /* Make sure CoreModule is imported only by one NgModule the AppModule */
@@ -38,6 +38,6 @@ export class CoreModule {
  * Factory to load tranlation from local json file (./assets/I18n/[lang].json)
  * @param http httpClient to load file with translates
  */
-export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http, './assets/i18n/');
-}
+// export function HttpLoaderFactory(http: HttpClient) {
+//   return new TranslateHttpLoader(http, './assets/i18n/');
+// }
